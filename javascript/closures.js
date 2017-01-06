5ac51702db7f6b77cec0dc896d9cd1ef1dc9facf
Danielle Blank <dgdblank@gmail.com>
2017-01-06 12:39:54 -0800

    add file about closures

---
+++ javascript/closures.js
@@ -0,0 +1,13 @@
+// How would you write times to give the output below? Why?
+function times(fn, x) {
+	
+}
+
+var calledOnce = function() {
+	console.log('1');
+};
+
+var calledTwice = times(calledOnce, 2);
+calledTwice(); \\ 1
+calledTwice(); \\ 1
+calledTwice(); undefined;
